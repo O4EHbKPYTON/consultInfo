8ca02268e8b1cb81c0e72e5149852adcf9fc007d
Yan <yan.matveyev@gmail.com>
2024-03-08 20:42:53 +0200

    refactored directories

--- consultInfo3/functions.php
+++ consultInfo3/functions.php
@@ -1,13 +1,6 @@
 <?php
-// function sumOfMultiplesOf5and3($n): int
-// {
-// 	$sum=0;
-// 	for ($i = 0; $i < $n; $i++)
-// 	{
-// 		if(($n%3===0)||($n%5===0))
-// 		{
-// 			$sum+=$n;
-// 		}
-// 	}
-// 	return $sum;
-// }
+function sumMultiples($limit, $multiple): float|int
+{
+	$n = floor(($limit - 1) / $multiple);
+	return $multiple * ($n * ($n + 1)) / 2;
+}
--- consultInfo3/index.php
+++ consultInfo3/index.php
@@ -1,12 +1,9 @@
 <?php
+declare(strict_types=1);
 
-function sumMultiples($limit, $multiple) {
-	$n = floor(($limit - 1) / $multiple);
-	return $multiple * ($n * ($n + 1)) / 2;
-}
+require_once "functions.php";
 
 $limit = 1000;
 $sum = sumMultiples($limit, 3) + sumMultiples($limit, 5) - sumMultiples($limit, 15);
 echo "Алгоритм работает за константное время O(1)" . PHP_EOL;
 echo $sum;
-?>
